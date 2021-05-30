7b8a8a19bc6a8731839907fbeabd849037c64e90
Glitch (glitch-hello-node) <none>
2021-05-30 16:49:47 +0000

    🍗🚣 Checkpoint ./rodzen.js:30380110/776

--- rodzen.js
+++ rodzen.js
@@ -17,19 +17,19 @@ for(const file of reader) {
 }
 bot.joinCommand({ //command
 channel: "$getServerVar[hgbbkanal]", //channel where it will log
-code: `> <@$authorID> $serverName sunucusuna Hoş geldin.
+code: `> **<@$authorID> $serverName sunucusuna Hoş geldin.**
 $attachment[https://api.xzusfin.repl.co/card?avatar=$replaceText[$authorAvatar;.webp;.png; ]&middle=welcome&name=$replaceText[$replaceText[$username[$authorID]#$discriminator[$authorID];#;%23; ]; ;%20; ]&bottom=$replaceText[Members Count $membersCount[$guildID]; ;%20;-1]&background=https://cdn.discordapp.com/attachments/797069150215602239/798428052392116224/images_20.jpeg&text=%23ffffff&avatarborder=%23FFFFFF&avatarbg=%23FF28b3]
 ` //Message sent to <channel>
 })
 bot.joinCommand({ //command
 channel: "$getServerVar[sayaçkanal]", //channel where it will log
-code: `> \`$usertag\` senle birlikte \`$membersCount\` kişi olduk. 
+code: `> :white_check_mark:** \`$usertag\` senle birlikte \`$membersCount\` kişi olduk. \`[$getServerVar[sayaç]\` kişi olmamıza  \`$sub[$getServerVar[sayaç];$membersCount]\` kişi kaldı.**
 ` //Message sent to <channel>
 })
 bot.joinCommand({ //command
 channel: "$getServerVar[otorolkanal]", //channel where it will log
-code: `> <@$authorID> $serverName sunucusuna Hoş geldin.
-$attachment[https://api.xzusfin.repl.co/card?avatar=$replaceText[$authorAvatar;.webp;.png; ]&middle=welcome&name=$replaceText[$replaceText[$username[$authorID]#$discriminator[$authorID];#;%23; ]; ;%20; ]&bottom=$replaceText[Members Count $membersCount[$guildID]; ;%20;-1]&background=https://cdn.discordapp.com/attachments/797069150215602239/798428052392116224/images_20.jpeg&text=%23ffffff&avatarborder=%23FFFFFF&avatarbg=%23FF28b3]
+code: `> :white_check_mark: **\`$usertag\` aramıza __\`$rolName[$getServerVar[otorol]]\`__ rolü ile katıldı.
+$giveRole[$authorID;$getServerVar[otorol]]
 ` //Message sent to <channel>
 })
 
