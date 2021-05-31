5f4d48ce229ded78dd941d7e6aebfc07e583a1b2
Glitch (glitch-hello-node) <none>
2021-05-31 15:41:15 +0000

    👩‍❤️‍💋‍👩🏞 Checkpoint ./komutlar/cüzdan.js:30380110/254

--- komutlar/cüzdan.js
+++ komutlar/cüzdan.js
@@ -1,4 +1,6 @@
 module.exports = {
         name: "cüzdan",//Buraya Kod İsmi Yazılır
-        code: `KOMUT`//Buraya Kodunuzu Yazınız
+        code: `
+        $attachment[https://nkcf66.sirv.com/bot/ACoins.png?text.0.text=$getUserVar[para;$mentioned[1;y]]&text.0.position.x=11%25&text.0.position.y=-43%25&text.0.color=000000&text.0.font.family=Lato]     
+        `//Buraya Kodunuzu Yazınız
 }
