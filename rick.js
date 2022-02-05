e86e8381d4ceb88d631fc61fefe3bf7e8eece66c
Kuchi999 <98208198+Kuchi999@users.noreply.github.com>
2022-02-06 01:14:03 +0300

    Update rick.js

--- rick.js
+++ rick.js
@@ -1,12 +1,16 @@
 const morty = require("aoi.js")
 var fs = require('fs')
 const bot = new morty.Bot({
-    token:process.env.token,
-    prefix:"."
+    token:"Token",
+    prefix:"Prefix"
 })
+
+
 bot.onJoined()
 bot.onLeave()
 bot.onMessage()
+
+
 var reader = fs.readdirSync("./komutlar/").filter(file => file.endsWith(".js"))
 for(const file of reader) {    
     const command = require(`./komutlar/${file}`)
@@ -16,20 +20,20 @@ for(const file of reader) {
     })
 }
 
+
 bot.command({
   name:"ping",
   code:`
-  Gecikmem : $ping | Bot gecikmesi : $botPing
+  \`\`\`
+  Gecikmem : $ping | Bot Gecikmesi : $botPing
+  \`\`\`
   `
 })
 
+
 bot.status({
-    text: `Owner `,
-    type: "PLAYING",
-    status: "dnd",
+    text: ``,
+    type: "LISTENING",
+    status: "idle",
     time: 12
 })
-
-bot.variables({
-rick:"morty",
-  })
